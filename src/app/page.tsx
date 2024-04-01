8e4bc89fd69ae1fbc74ae123411dfe1bd0a5c7e8
Marshall Kurniawan <shrallvierdo@gmail.com>
2024-04-01 16:53:42 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [cookie, setCookie] = useState("");
+  useEffect(() => {
+    if (Cookies.get("token") === undefined) {
+      const tokenValue = "Token-" + Math.floor(Math.random() * 1000 + 1);
+      Cookies.set("token", tokenValue, {
+        domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+      });
+      setCookie(tokenValue);
+    } else {
+      const tokenValue = Cookies.get("token");
+      if (tokenValue) setCookie(tokenValue);
+    }
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Cookie: {cookie}</div>
+    </main>
+  );
+}
